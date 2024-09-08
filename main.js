30f1a52cef5638e59d216cf66dd88cb827429bd2
Irving-8man <geyler0502@outlook.com>
2024-09-08 16:53:49 -0600

    prueba de renderizar modelo

--- main.js
+++ main.js
@@ -1,5 +1,73 @@
-import * as THREE from 'three'
+import * as THREE from "three"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 1, 2);
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enablePan = false;
+controls.minDistance = 2;
+controls.maxDistance = 10;
+
+let model;
+const loader = new GLTFLoader();
+
+loader.load('./models/cuencaMaya2.glb', (gltf) => {
+    model = gltf.scene;
+    console.log('Model loaded:', model);
+    scene.add(model);
+
+    model.traverse((child) => {
+        if (child.isMesh) {
+            console.log('Applying standard material to mesh:', child);
+            
+        }
+    });
+});
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(2, 2, 5);
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const rotationSpeed = 0.28; // radians per second
+const clock = new THREE.Clock();
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    const delta = clock.getDelta();
+    if (model) {
+        model.rotation.y += rotationSpeed * delta;
+    }
+    renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener("resize", () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+});
+
+/** 
 //Scene
 const scena = new THREE.Scene()
 
@@ -37,7 +105,14 @@ cube3.position.x=2
 group.add(cube3)
 
 
-/** 
+///AQUI si sirve
+*/
+
+
+
+
+
+/*
 
 //Cubo rojo
 const geometry = new THREE.BoxGeometry(1,1,1)
@@ -68,6 +143,10 @@ mesh.rotation.y= Math.PI*.25
 */
 
 
+
+/** 
+ * 
+ * AQUI si sirve
 //Axes helpers
 const axishelper = new THREE.AxesHelper(2)
 scena.add(axishelper)
@@ -96,3 +175,4 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width,sizes.height)
 
 renderer.render(scena,camera)
+*/
